4da8244a1353b2508cfbc67c236c0b00030034ec
EMILLAYNNE VOCTORIA DA SILVA <emillaynne_silva@estudante.sesisenai.org.br>
2024-03-06 15:34:30 -0300

    soma,subtracao e multiplicacao no back end

---
+++ principal_array3.js
@@ -0,0 +1,38 @@
+const readlineSync = require('readline-sync')
+
+let numArray1 = [1, 2, 3, 4, 5]
+let numArray2 = []
+let numArray3 = []
+
+let valor = 0
+
+function mult(){
+    for(let i=0;i<=4;i++){
+        numArray3[i] = numArray1[i] *  numArray2[i]
+    
+
+}
+return numArray3
+}
+
+function ler(){
+    let contador = 0
+
+    while(contador <= 4 ){
+        valor = Number(readlineSync.question('Digite um valor para ser guardado: '))
+        numArray2.push(valor)
+        contador += 1
+    }
+    return numArray2
+
+}
+
+console.log('======================================')
+console.log(' Multiplicacao dos valores de 2 arrays ')
+console.log('======================================')
+
+let leia = ler()
+let resposta = mult()
+
+console.log('O array 2 é =',leia)
+console.log('O array 3 é =',resposta)
